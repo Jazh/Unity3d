65929520e92c72c274b7302a5f14ba0ba455e9ad
unknown <aguione@gmail.com>
2020-02-27 22:12:30 -0500

    update example

---
+++ Assets/Scripts/FPTD/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPTD
+{
+    public class CameraController : MonoBehaviour
+    {
+        public bool invert = false;
+        public float smooth = 10f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //Quaternion rot = transform.rotation;
+            //Debug.Log(rot.eulerAngles);
+            //transform.rotation = rot;
+        }
+
+        // Update is called once per frame
+        void LateUpdate()
+        {
+            float angle = Input.GetAxis("Mouse Y");
+            angle *= smooth * Time.deltaTime;
+
+            if (invert) {
+                angle *= -1;
+            }
+
+           // if (Input.GetAxis("Mouse Y") != 0f) {
+                transform.Rotate(Vector3.right, angle);
+            //}
+
+            Quaternion rot = transform.rotation;
+            Vector3 angel = rot.eulerAngles;
+
+            //angle.x = Mathf.Clamp(rot.eulerAngles.x, -50f, 50);
+
+        }
+    }
+}
